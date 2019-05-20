f9f10bd0b7f9ea6bd5e7a44da5a0bfd00a6ee9ca
ypq <hrbypq@163.com>
2019-05-20 16:15:41 +0800

    修改了工具类方法

--- src/controller/Tools.java
+++ src/controller/Tools.java
@@ -1,16 +1,45 @@
 package controller;
 
+import java.util.*;
+
 /**
  * 工具类
  * @author
  * @version 1.0
  */
 public class Tools {
+
 	/**
 	 * 构造方法
 	 */
 	public Tools() {
-		// TODO Auto-generated constructor stub
 	}
+
+	/**
+	 * 输入指定范围的整数  用来选择菜单中的选项
+	 * @param lo 起始数字
+	 * @param hi 终止数字
+	 * @return 输入值
+	 */
+	public static int inputInteger(int lo,int hi) {
+		int input=0;
+		Scanner scanner=new Scanner(System.in);
+		while(true) {
+			try {
+				input=scanner.nextInt();
+				if(input<lo||input>hi) 
+					throw new Exception();
+			}
+			catch(Exception e) {
+				System.out.println("输入有误，请重新输入");
+				scanner=new Scanner(System.in);
+				continue;
+			}
+			break;
+		}
+		scanner.close();
+		return input;
+	}
+	
 	
 }
